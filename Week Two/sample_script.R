d04c55846ad3ff6866e8378a7cfceb8f75294087
PhilLeft <philip.leftwich@gmail.com>
2021-09-30 11:38:18 +0100

    Week two slides

---
+++ Week Two/sample_script.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+class_RT <- read_csv()
+
+names(class_RT) <- c("timestamp",
+                     "id",
+                     "hand",
+                     "react_1",
+                     "react_2",
+                     "react_3",
+                     "react_4",
+                     "react_5",
+                     "verbal",
+                     "number_memory",
+                     "visual_memory",
+                     "activity")
+
+ type_convert()
